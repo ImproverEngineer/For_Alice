fda991cec8d012cda5a5c62cd6001a138b70eea2
RinatMagnat <50412433+RinatMagnat@users.noreply.github.com>
2022-02-11 21:10:04 +0500

    1 Программа для записи трейса выполненных шагов.

---
+++ TraceProgram/TraceProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TraceProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SaveToTXT saveToFile = new SaveToTXT(false);
+            int N = 0;                                    // значение N
+            saveToFile.SaveText( "N = " + N);            
+            int I = 0;                                    // значение I
+            saveToFile.SaveText( "I = " + I.ToString());
+            int P = 0;                                    // незнаю зачем она тут
+            saveToFile.SaveText("P = " + P.ToString());
+            while (I <= 10)
+            {              
+                saveToFile.SaveText(" Пока i<=10 ", ConsoleColor.Red);
+                saveToFile.SaveText(" N + I",   ConsoleColor.Red);
+                saveToFile.SaveText(" I + 1",   ConsoleColor.Red);
+                saveToFile.SaveText(" N = " + N, ConsoleColor.Green);                
+                saveToFile.SaveText(" I = " + I, ConsoleColor.Green);                
+                N += I;
+                I++;
+            }
+            saveToFile.SaveText(string.Format(" Конец N ={0}, I ={1}",N,I));
+            saveToFile.StreamClose();
+        }
+    }
+}
---
+++ TraceProgram/TraceProgram/SaveToTXT.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TraceProgram
+{
+    class SaveToTXT
+    {
+        string st; // путь к текушей папке  
+        StreamWriter sw; // запись к файлу
+        static int count;
+        public SaveToTXT(bool overwriting)
+        {
+            count = 0;
+            st = Directory.GetCurrentDirectory();
+            if (Path.HasExtension(st))
+            {
+                sw = new StreamWriter(Path.Combine(st,"Trace.txt"), overwriting, Encoding.UTF8);
+            }  
+        }
+        public void SaveText(string text,ConsoleColor consoleColor = ConsoleColor.White) 
+        {
+            try
+            {
+                sw.WriteLine(count.ToString()+" "+text);
+                ConsoleWrite(count.ToString() + " " + text, consoleColor);
+                count++;
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(count.ToString() +" "+"Ошибка работы с файлом" + ex.Message);
+                sw.Close();
+            }
+        }
+        public void StreamClose() 
+        {
+            sw.Close();
+        }
+        public void ConsoleWrite(string text, ConsoleColor cc = ConsoleColor.White) 
+        {
+            ConsoleColor consoleColor = Console.ForegroundColor;
+            Console.ForegroundColor = cc;
+            Console.WriteLine(text);
+            Console.ForegroundColor = consoleColor;
+        }
+    }
+}
